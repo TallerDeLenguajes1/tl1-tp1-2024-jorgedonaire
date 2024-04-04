5520d0af5fdd6c9b3d9d52267ac091ed82ffd31d
Jorge Gregorio Donaire Pereyra <jorgedonaire93@gmail.com>
2024-04-04 11:34:06 -0300

    ejercicio 2.4 completado

--- tp1_2.cpp
+++ tp1_2.cpp
@@ -5,10 +5,32 @@ int cuadradoNumero(int a);
 void cuadrado(int a);
 void mostrarVariable(int a);
 void invertir(int a, int b);
+void orden(int a, int b);
+
 int main(){
 
-    return 0;
+    cuadrado(4);
+    mostrarVariable(5);
 
+    //Prueba de funcion invertir
+    cout << "Funcion invertir" << endl;
+    int a,b;
+    cout << "Ingresar el valor a: " << endl;
+    cin >> a;
+    cout << "Ingresar el valor b: " << endl;
+    cin >> b;
+    invertir(a,b);
+
+    //Prueba de funcion orden
+    cout << "Funcion orden" << endl;
+    cout << "Ingresar el valor a: " << endl;
+    cin >> a;
+    cout << "Ingresar el valor b: " << endl;
+    cin >> b;
+    orden(a,b);
+
+
+    return 0;
 }
 
 int cuadradoNumero(int a){
@@ -20,11 +42,33 @@ void cuadrado(int a){
 }
 
 void mostrarVariable(int a){
+    // cout << "Ingresar el valor de la variable a: " << endl;
+    // cin >> a;
     cout << "La direccion de memoria de la variable es: " << &a << endl;
     cout << "El contenido de la variable es: " << a << endl;
 }
 
 void invertir(int a, int b){
-    cin >> "Ingresar el valor a: " << a << endl;
-    cin
+    int temp;
+    int *aux;
+    aux = &a;
+    temp=*aux;
+    
+    *aux=b;
+    b = temp;
+    
+    cout << "La variable a es: " << a << endl;
+    cout << "La variable b es: " << b << endl;
+}
+
+void orden(int a, int b){
+    if (a > b)
+    {
+        invertir(a,b);
+    }else
+    {
+        cout << "La variable a es: " << a << endl;
+        cout << "La variable b es: " << b << endl;
+    }
+    
 }
